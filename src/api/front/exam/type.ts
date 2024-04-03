c605e2eddd9bfbc80afa5550bf9021e544ed0910
wcurly <jiarule09@gmail.com>
2024-04-03 22:29:19 +0800

    美化页面；重新修改主页

--- src/api/front/exam/type.ts
+++ src/api/front/exam/type.ts
@@ -1,54 +1,55 @@
 export interface ResponseData {
-    code: number;
-    message: string;
+  code: number;
+  message: string;
 }
 
 //前台的考试列表
 interface TestItem {
-    exam_id: string,
-    paper_id: string,
-    exam_name: string,
-    duration: number,
-    exam_start: string|null,
-    exam_end: string|null,
+  exam_id: string;
+  paper_id: string;
+  exam_start: string | null;
+  exam_end: string | null;
+  exam_name: string;
+  duration: number;
 }
 
-export type TestListArr=TestItem[];
+export type TestListArr = TestItem[];
 
 export interface TestListResponseData extends ResponseData {
-    data: TestListArr;
+  data: TestListArr;
 }
 
 //试卷详情
-interface optItem{
-    optCode: string;
-    optContents: string;
+interface optItem {
+  optCode: string;
+  optContents: string;
 }
 
 export interface questionItem {
-    question_id: string,
-    order: string,
-    question_body:string,
-    type: string,
-    options: optItem[],
-    right_choice: string,
-    value: number,
-    // selectedOpt: null|string,
-    // status: string,
+  question_id: string;
+  order: string;
+  question_body: string;
+  type: string;
+  options: optItem[];
+  right_choice: string;
+  value: number;
+  // selectedOpt: null|string,
+  // status: string,
 }
 
-export type QuestionListArr=questionItem[];
+export type QuestionListArr = questionItem[];
 
-export interface TestDetail{
-    paper_id: string,
-    paper_name: string,
-    duration: number,
-    question_number: number,
-    value: number,
-    questions: QuestionListArr;
-    selectedOpt: null|string;
-    status: string;
+export interface TestDetail {
+  paper_id: string;
+  paper_name: string;
+  duration: number;
+  question_id: null | string;
+  question_number: number;
+  question_value: number;
+  questions: QuestionListArr;
+  selectedOpt: null | string;
+  status: string;
 }
 export interface TestDetailResponseData extends ResponseData {
-    data: TestDetail;
+  data: TestDetail;
 }
